6085dfd492fc4e5d45b285a04b1fac7259ffa92a
Marat Mutalipov <maratios41@gmail.com>
2023-03-23 23:10:05 +0300

    Styling

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ const NextQuestion = () => {
     <section class="quiz" v-if="!quizCompleted">
       <div class="quiz-info">
         <span class="question">
-          {{ getCurrentQuestion.question }}
+          Вопрос: {{ getCurrentQuestion.question }}
         </span>
         <span class="score">Счет {{ score }}/{{ questions.length }}</span>
       </div>
@@ -123,4 +123,89 @@ body {
   color: #fff;
 }
 
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  height: 100vh;
+}
+h1 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+.quiz {
+  background-color: #382a4b;
+  padding: 1rem;
+  width: 100%;
+  max-width: 640px;
+}
+.quiz-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+.quiz-info .question {
+  color: #8f8f8f;
+  font-size: 1.25rem;
+}
+.quiz-info.score {
+  color: #fff;
+  font-size: 1.25rem;
+}
+.options {
+  margin-bottom: 1rem;
+}
+.option {
+  padding: 1rem;
+  display: block;
+  background-color: #271c36;
+  margin-bottom: 0.5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+.option:hover {
+  background-color: #2d213f;
+}
+.option.correct {
+  background-color: #2cce7d;
+}
+.option.wrong {
+  background-color: #ff5a5f;
+}
+.option:last-of-type {
+  margin-bottom: 0;
+}
+.option.disabled {
+  opacity: 0.5;
+}
+.option input {
+  display: none;
+}
+button {
+  appearance: none;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  background-color: #2cce7d;
+  color: #2d213f;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+}
+button:disabled {
+  opacity: 0.5;
+}
+h2 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+p {
+  color: #8f8f8f;
+  font-size: 1.5rem;
+  text-align: center;
+}
 </style>
